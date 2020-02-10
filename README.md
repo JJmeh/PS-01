c5646b8adeef664c81c8b5e5616b38942a3f77f9
Jeremiah Jason <jeremiah.kaliki@gmail.com>
2020-02-10 14:56:50 +0700

    First Upload

---
+++ README.md
@@ -0,0 +1,41 @@
+This folder contain app for development server
+
+extension of project PP-01 or codenamed BILL
+
+for html request and receive stuff
+
+PS-01
+
+Python Server 01
+
+notes :
+
++ get comp status: ( Status )
+	-CPU temp
+	-GPU temp
+	-CPU percentage usage
+	-ngrok status ( tcp )
+	-ngrok status ( html )
+	-storage percentage
+	-charge status
+
++ status format
+
+	======================
+	CPU percentage :
+	CPU temp :
+	GPU temp :
+	storage percentage :
+	======================
+
+
++ reboot command
+
++ shutdown command
+
+notes of command ( and to install )
+
+nvidia-smi
+mpstat
+
+df --output=pcent / | awk -F '%' 'NR==2{print $1}'
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, abort
+
+import json
+
+import subprocess
+
+app = Flask(__name__)
+
+pwd = '1910'
+
+@app.route("/data", methods=['POST'])
+def data():
+    data = json.loads(request.data, strict=False)
+    print("data received '{}'".format(str(data)))
+    if data == 'Status':
+        # sent data with curl to heroku app
+        cpuPercentage = subprocess.getoutput("mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12\"%\" }'")
+        cpuTemp = subprocess.getoutput('echo {} | sudo -S {}'.format(pwd, 'tlp-stat -t | grep temp | awk \'{print $4}\''))
+        gpuTemp = subprocess.getoutput('nvidia-smi | grep C | awk "{print $2}"')
+        storagePercent = subprocess.getoutput('df --output=pcent / | awk -F "%" "NR==2{print $1}"')
+        print('\nCPU percentage : {}\nCPU temperature : {} C\nGPU Temperature : {}\nStorage Percentage : used {}\n'.format(cpuPercentage, cpuTemp, gpuTemp, storagePercent))
+    return data
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)  
